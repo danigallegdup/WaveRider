a8f4c6eece75281594a59a06d31df23ffe756c16
Noah <noahedjacobsen@gmail.com>
2025-03-26 23:21:20 -0700

    Redid layout of UI for responsiveness; added font

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -6,19 +6,23 @@ const TIME_PREFIX = "Time: "
 
 var score = 0
 
+@onready var health_label = $MarginContainer/HBoxContainer/HealthLabel
+@onready var time_label = $MarginContainer/HBoxContainer/TimeLabel
+@onready var score_label = $MarginContainer/HBoxContainer/ScoreLabel
+
 func initialize():
 	set_score(0)
 	set_time(0.0)
 	set_health(5)
 
 func set_health(new_health):
-	$HealthLabel.text = HEALTH_PREFIX + str(new_health)
+	health_label.text = HEALTH_PREFIX + str(new_health)
 	
 func set_time(new_time):
-	$TimeLabel.text = TIME_PREFIX + str(new_time)
+	time_label.text = TIME_PREFIX + str(new_time)
 
 func set_score(new_score=score):
-	$ScoreLabel.text = SCORE_PREFIX + str(score)
+	score_label.text = SCORE_PREFIX + str(score)
 func add_score(new_score):
 	score += new_score
 	set_score()
