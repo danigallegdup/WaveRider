8cd49e6a2f4b1b16e11c2c7643c4a5d4f3fbf1e2
Noah <noahedjacobsen@gmail.com>
2025-03-29 23:28:59 -0700

    Added pausing

--- Scripts/menus.gd
+++ Scripts/menus.gd
@@ -17,7 +17,9 @@ This script will be used to power menu transitions, game start, and pauses.
 @onready var cur_menu: Control = menus.main
 
 func _ready():
-	switch_menu(menus.main)
+	#switch_menu(menus.main)
+	switch_menu(menus.pause)
+	self.hide()
 	
 func switch_menu(target:Control):
 	if cur_menu.name == target.name: return
@@ -25,3 +27,18 @@ func switch_menu(target:Control):
 	cur_menu.hide()
 	target.show()
 	cur_menu = target
+	
+func toggle_pause():
+	switch_menu(menus.pause)
+	if self.visible:
+		# Do unpause
+		self.hide()
+		TimescaleUtil.toggle_pause()
+	else:
+		# Do pause
+		self.show()
+		TimescaleUtil.toggle_pause()
+
+
+func _on_continue_button_down() -> void:
+	toggle_pause()
