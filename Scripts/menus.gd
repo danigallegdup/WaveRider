2608810e1a5fcf38512b1634d61236e581d82fba
Noah <noahedjacobsen@gmail.com>
2025-03-30 14:08:18 -0700

    Added very simple song list

--- Scripts/menus.gd
+++ Scripts/menus.gd
@@ -13,13 +13,18 @@ This script will be used to power menu transitions, game start, and pauses.
 		"pause": $Pause,
 		"game_over": $GameOver,
 	}
-
 @onready var cur_menu: Control = menus.main
+
+@onready var song_list = $SongSelect/ScrollContainer/SongList
+@onready var song_list_item = preload("res://Scenes/SongListItem.tscn")
 
 func _ready():
 	#switch_menu(menus.main)
-	switch_menu(menus.pause)
-	self.hide()
+	switch_menu(menus.song_select)
+	#self.hide()
+	# TODO Load MusicLoader.song_library into song_list
+	create_list_item({"song_name": "Wowee"})
+	connect_song_list_buttons()
 	
 func switch_menu(target:Control):
 	if cur_menu.name == target.name: return
@@ -39,6 +44,18 @@ func toggle_pause():
 		self.show()
 		TimescaleUtil.toggle_pause()
 
+func create_list_item(song_data):
+	var l = song_list_item.instantiate()
+	l.find_child("SongName").text = song_data.song_name
+	song_list.add_child(l)
+
+func connect_song_list_buttons():
+	for b in song_list.get_children():
+		#f.bind(b.find_child("SongName").text)
+		b.find_child("Button").connect("button_down", _on_song_list_item_button_down.bind(b.find_child("SongName").text))
 
 func _on_continue_button_down() -> void:
 	toggle_pause()
+
+func _on_song_list_item_button_down(song_name):
+	print(song_name)
